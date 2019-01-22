485c30d0373c2ce6207eafbbdb158f7e643f863a
diodonfrost <diodon.frost@diodonfrost.me>
2019-01-22 11:40:40 +0100

    setup bind service

--- tests/inspec/install_spec.rb
+++ tests/inspec/install_spec.rb
@@ -3,11 +3,24 @@
 bind_package = 'bind'
 bind_package = 'bind9' if os.family == "debian"
 
+bind_service = 'named'
+bind_service = 'bind9' if os.family == "debian"
+
 control 'install-01' do
   impact 1.0
   title 'Bind install'
   desc 'Bind should be installed'
   describe package(bind_package) do
     it { should be_installed }
+  end
+end
+
+control 'install-02' do
+  impact 1.0
+  title 'Bin service'
+  desc 'Bind service shoud be enabled and started'
+  describe service(bind_service) do
+    it { should be_enabled }
+    it { should be_running }
   end
 end
